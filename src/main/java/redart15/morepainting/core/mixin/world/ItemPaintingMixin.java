1dd9956971dca24b5de8a12899c442e3e31d082e
Redart15 <redart15@users.noreply.github.com>
2026-08-31 00:29:58 +0200

    painting now correctly sets heading

--- src/main/java/redart15/morepainting/core/mixin/world/ItemPaintingMixin.java
+++ src/main/java/redart15/morepainting/core/mixin/world/ItemPaintingMixin.java
@@ -6,14 +6,8 @@ import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.entity.EntityPainting;
 import net.minecraft.core.entity.player.Player;
 import net.minecraft.core.item.ItemPainting;
-import net.minecraft.core.item.ItemStack;
-import net.minecraft.core.util.helper.Side;
-import net.minecraft.core.world.World;
-import net.minecraft.core.world.pos.TilePosc;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import redart15.morepainting.client.Index;
 import redart15.morepainting.core.interfaces.PaintingIndex;
 import redart15.morepainting.core.interfaces.PlayerAdditionalGui;
@@ -26,21 +20,19 @@ public class ItemPaintingMixin {
 		((PlayerAdditionalGui)player).morepainting$displayScreenPicker();
 	}
 
-	@Inject(method = "onUseOnBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/entity/EntityPainting;canStay()Z", shift = At.Shift.BEFORE))
-	private void adjustEnitity(
-		ItemStack selfStack,
-		World world,
-		Player player,
-		TilePosc blockPos,
-		Side side, double xHit, double yHit,
-		CallbackInfoReturnable<Boolean> cir,
+	@WrapOperation(method = "onUseOnBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/entity/EntityPainting;canStay()Z"))
+	private boolean adjustEnitity(
+		EntityPainting instance,
+		Operation<Boolean> original,
 		@Local EntityPainting entityPainting,
-		@Local int direction
+		@Local int direction,
+		@Local(argsOnly = true) Player player
 	){
 		if(entityPainting instanceof PaintingIndex entityIndex && player instanceof  PaintingIndex playerIndex){
 			Index index = playerIndex.morepainting$getIndex();
 			entityIndex.morepainting$setIndex(index);
 			entityPainting.setDirection(direction);
 		}
+		return original.call(instance);
 	}
 }
